54a7308837b62f617bbcc79f8fd0a7888d20d642
Bymarium <bymarium@gmail.com>
2024-10-17 15:21:05 -0500

    update name url userController

--- src/main/java/com/example/task/appTask/controller/UserController.java
+++ src/main/java/com/example/task/appTask/controller/UserController.java
@@ -18,33 +18,33 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping
+    @PostMapping("/saveUser")
     public ResponseEntity<CreateUserDto> saveUser(@RequestBody CreateUserDto userDto) {
         return new ResponseEntity<CreateUserDto>(userService.saveUser(userDto), HttpStatus.CREATED);
     }
 
     //GetAll Rest Api
-    @GetMapping
+    @GetMapping("/getAllUsers")
     public ResponseEntity<List<GetUserDto>> getAllUsers() {
         List<GetUserDto> users = userService.getAllUsers();
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
     //Get by document Rest Api
-    @GetMapping("{document}")
+    @GetMapping("/getUserById/{document}")
     //localhost:8080/api/users/1
     public ResponseEntity<GetUserDto> getUserById(@PathVariable("document") long userDocument) {
         return new ResponseEntity<GetUserDto>(userService.getUserById(userDocument), HttpStatus.OK);
     }
 
     //Update Rest Api
-    @PutMapping("{document}")
+    @PutMapping("/updateUser/{document}")
     public ResponseEntity<UpdateUserDto> updateUser(@PathVariable("document") long document, @RequestBody UpdateUserDto userDto) {
         return new ResponseEntity<UpdateUserDto>(userService.updateUser(userDto, document), HttpStatus.OK);
     }
 
     //Delete Rest Api
-    @DeleteMapping("{document}")
+    @DeleteMapping("/deleteUser/{document}")
     public ResponseEntity<DeleteUserDto> deleteUser(@PathVariable("document") long document) {
         //delete user from database
         DeleteUserDto userDto = userService.deleteUser(document);
